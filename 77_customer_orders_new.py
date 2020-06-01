651c9a86c2927188b31bf95b5d365031bbba1af2
paul <paul.jose@confianzit.biz>
2020-06-01 09:47:23 +0530

    open order and details

---
+++ 77_customer_orders_new.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+import multiprocessing as mp
+import xmlrpc.client as xmlrpclib
+import queue
+
+from scriptconfig import URL, DB, UID, PSW, WORKERS
+
+
+# ==================================== SALE ORDER ====================================
+
+def update_sale_order(pid, data_pool, error_ids, partner_ids, term_ids):
+    while True:
+        try:
+            sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+            data = data_pool.get_nowait()
+        except queue.Empty:
+            break
+        try:
+            order_no = data.get('ref', '')
+            order_list = data.get('orders', [])
+
+            partner_id = partner_ids.get(order_list[0].get('CUSTOMER-CODE', '').strip())
+            term_id = term_ids.get(order_list[0].get('TERM-CODE', '').strip())
+            if not partner_id or not term_id:
+                error_ids.append(order_no)
+                continue
+
+            vals = {
+                'name': order_list[0].get('ORDER-NO', '').strip(),
+                'partner_id': partner_id,
+                'payment_term_id': term_id,
+                'date_order': order_list[0].get('ORDER-DATE', '').strip(),
+            }
+
+
+            try:
+                # Check if order exists
+                res = sock.execute(DB, UID, PSW, 'sale.order', 'search_read', [('name', '=', order_no)])
+                if res:
+                    order_id = res[0]['id']
+                    try:
+                        # If order exists, remove followers to prevent Odoo from trying to duplicate
+                        message_partner_ids = res[0]['message_partner_ids']
+                        if message_partner_ids:
+                            sock.execute(DB, UID, PSW, 'mail.followers', 'unlink', message_partner_ids)
+                        print ('bb')
+                    except Exception as e:
+                        print(e)
+                        pass
+                    # Update the DB
+                    sock.execute(DB, UID, PSW, 'sale.order', 'write', order_id, vals)
+                    print(pid, 'UPDATE - SALE ORDER', order_id, res[0]['name'])
+                else:
+                    res = sock.execute(DB, UID, PSW, 'sale.order', 'create', vals)
+                    print(pid, 'CREATE - SALE ORDER', res, order_no)
+            except Exception as e:
+                data_pool.put(data)
+        finally:
+            data_pool.task_done()
+
+def sync_sale_orders():
+    manager = mp.Manager()
+    data_pool = manager.JoinableQueue()
+    error_ids = manager.list()
+
+    orders = {}
+    with open('files/omlordr1.csv', newline='') as f:
+        csv_reader = csv.DictReader(f)
+        for vals in csv_reader:
+            order_no = vals['ORDER-NO'].strip()
+            orders.setdefault(order_no, [])
+            orders[order_no].append(vals)
+
+    for ref in orders:
+        data_pool.put({'ref': ref, 'orders': orders[ref]})
+
+    sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+    res = sock.execute(DB, UID, PSW, 'res.partner', 'search_read', ['|', ('active', '=', False), ('active', '=', True)],
+                       ['customer_code'])
+    customers = {rec['customer_code']: rec['id'] for rec in res}
+    partner_ids = manager.dict(customers)
+
+    payment_terms = sock.execute(DB, UID, PSW, 'account.payment.term', 'search_read', [('order_type', '=', 'sale')],
+                                 ['id', 'code'])
+    term_ids = {term['code']: term['id'] for term in payment_terms}
+    workers = []
+    for i in range(WORKERS):
+        pid = "Worker-%d" % (i + 1)
+        worker = mp.Process(name=pid, target=update_sale_order,
+                            args=(pid, data_pool, error_ids, partner_ids, term_ids))
+        worker.start()
+        workers.append(worker)
+
+    data_pool.join()
+
+if __name__ == "__main__":
+    # SALE ORDER
+    sync_sale_orders()
---
+++ 78_customer_order_details_new.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+import csv
+import xmlrpc.client as xmlrpclib
+import multiprocessing as mp
+
+from scriptconfig import URL, DB, UID, PSW, WORKERS
+
+# ==================================== SALE ORDER LINE ====================================
+
+def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids):
+    sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+    while data_pool:
+        # try:
+            data = data_pool.pop()
+            order_id = data.get('order_id')
+            order_lines = sock.execute(DB, UID, PSW, 'sale.order.line', 'search_read', [('order_id','=',order_id)], ['product_id', 'product_uom'])
+            order_line_ids = {rec['product_id'][0]: rec['product_uom'][0]  for rec in order_lines}
+
+
+            for line in data.get('lines', []):
+                product_id = product_ids.get(line.get('ITEM-CODE', '').strip())
+                print (product_id)
+                code = str(line.get('ORDERING-UOM')).strip() + '_' + str(line.get('QTY-IN-ORDERING-UM')).strip()
+                code = uom_ids.get(code)
+
+                if not product_id and not code:
+                    error_ids.append()
+                    continue
+                if product_id in order_line_ids and code == order_line_ids[product_id]:
+                    # print('Duplicate')
+                    continue
+
+                vals = {
+                        'order_id': order_id,
+                        'product_id': product_id,
+                        'name': line.get('ITEM-DESC').strip(),
+                        'price_unit': line.get('PRICE-DISCOUNTED').strip(),
+                        'product_uom_qty': line.get('QTY-ORDERED').strip(),
+                        'is_last':False,
+                        'working_cost':line.get('TRUE-FIXED-COST').strip(),
+                        'lst_price':line.get('PRICE-DISCOUNTED').strip(),
+                        'product_uom': code,
+                        }
+                # print(vals)
+
+                res = sock.execute(DB, UID, PSW, 'sale.order.line', 'create', vals)
+                print(pid, 'Create - SALE ORDER LINE', order_id , res)
+
+        # except:
+        #     break
+
+
+def sync_sale_order_lines():
+    manager = mp.Manager()
+    data_pool = manager.list()
+    error_ids = manager.list()
+    process_Q = []
+
+    sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+    res = sock.execute(DB, UID, PSW, 'sale.order', 'search_read', [], ['name'])
+    order_ids = {rec['name'] : rec['id']  for rec in res}
+
+    fp = open('files/omlhist2.csv', 'r')
+    # fp = open('omlordr2.csv', 'r')
+    csv_reader = csv.DictReader(fp)
+    print('Opened File')
+
+    order_lines = {}
+    for vals in csv_reader:
+        ord_no = vals.get('ORDER-NO', '').strip()
+        order_id = order_ids.get(ord_no)
+        if order_id:
+            lines = order_lines.setdefault(order_id, [])
+            lines.append(vals)
+
+    fp.close()
+    print('Closed File')
+
+    data_pool = manager.list([{'order_id': order, 'lines': order_lines[order]} for order in order_lines])
+
+
+    res = sock.execute(DB, UID, PSW, 'product.product', 'search_read', ['|', ('active', '=', False), ('active', '=', True)], ['default_code'])
+    products = {rec['default_code']: rec['id']  for rec in res}
+    product_ids = manager.dict(products)
+
+    uoms = sock.execute(DB, UID, PSW, 'uom.uom', 'search_read', [], ['id','name'])
+    uom_ids = {uom['name']:uom['id'] for uom in uoms}
+
+    res = None
+    order_ids = None
+    order_lines = None
+    products = None
+
+    for i in range(WORKERS):
+        pid = "Worker-%d" % (i + 1)
+        worker = mp.Process(name=pid, target=update_sale_order_line, args=(pid, data_pool, error_ids, product_ids, uom_ids))
+        process_Q.append(worker)
+        worker.start()
+
+    for worker in process_Q:
+        worker.join()
+
+
+
+if __name__ == "__main__":
+
+    # SALE ORDER LINE
+    sync_sale_order_lines()
