7ea37e4641f26a23f3e444eae271b48994331a41
paul <paul.jose@confianzit.biz>
2020-07-03 10:43:19 +0530

    bug fix in customer category

--- 19_customer_category_from_customers.py
+++ 19_customer_category_from_customers.py
@@ -11,23 +11,20 @@ socket = xmlrpc.client.ServerProxy(url,context=ssl._create_unverified_context())
 
 
 
-categs = socket.execute(db, 2, pwd, 'res.partner.category', 'search_read', [], ['id','name'])
-categories = {categ['name']: categ['id'] for categ in categs}
+categs = socket.execute(db, 2, pwd, 'res.partner.category', 'search_read', [], ['id','code'])
+categories = {categ['code']: categ['id'] for categ in categs}
 
 
 
 input_file = csv.DictReader(open("files/rclcust1.csv"))
 
 with open("Catg_ERROR.csv", "w") as f:
-    vals={}
     for line in input_file:
-        vals[line.get('CLASS-CODE').strip()] = 1
-
-    for val in vals:
         try:
-            if val not in categories:
-                print (val)
-                status = socket.execute(db, 2, pwd, 'res.partner.category', 'create', {'name': val})
+            code = line.get('CLASS-CODE').strip()
+            if code not in categories:
+                status = socket.execute(db, 2, pwd, 'res.partner.category', 'create', {'name': code,'code':code})
+                categories[code] = status
                 print (status)
         except:
             f.write(line.get('CLASS-CODE').strip())
