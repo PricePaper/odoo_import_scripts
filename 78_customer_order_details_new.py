14e12c6874e1277b4ad4cdbe46f6b454b43c2aec
paul <paul.jose@confianzit.biz>
2020-08-14 16:37:19 +0530

    tax in order_lines

--- 78_customer_order_details_new.py
+++ 78_customer_order_details_new.py
@@ -11,10 +11,10 @@ from scriptconfig import URL, DB, UID, PSW, WORKERS
 
 # ==================================== SALE ORDER LINE ====================================
 
-def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids):
+def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids, tax_code_ids, tax_ids, order_tax_code_ids):
     sock = xmlrpclib.ServerProxy(URL, allow_none=True)
     while data_pool:
-        # try:
+        try:
             data = data_pool.pop()
             order_id = data.get('order_id')
             order_lines = sock.execute(DB, UID, PSW, 'sale.order.line', 'search_read', [('order_id','=',order_id)], ['product_id', 'product_uom'])
@@ -23,7 +23,6 @@ def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids):
 
             for line in data.get('lines', []):
                 product_id = product_ids.get(line.get('ITEM-CODE', '').strip())
-                print (product_id)
                 code = str(line.get('ORDERING-UOM')).strip() + '_' + str(line.get('QTY-IN-ORDERING-UM')).strip()
                 code = uom_ids.get(code)
 
@@ -31,7 +30,6 @@ def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids):
                     error_ids.append()
                     continue
                 if product_id in order_line_ids and code == order_line_ids[product_id]:
-                    # print('Duplicate')
                     continue
 
                 vals = {
@@ -40,18 +38,22 @@ def update_sale_order_line(pid, data_pool, error_ids, product_ids, uom_ids):
                         'name': line.get('ITEM-DESC').strip(),
                         'price_unit': line.get('PRICE-DISCOUNTED').strip(),
                         'product_uom_qty': line.get('QTY-ORDERED').strip(),
-                        'is_last':False,
+                        'is_last': False,
                         'working_cost':line.get('TRUE-FIXED-COST').strip(),
                         'lst_price':line.get('PRICE-DISCOUNTED').strip(),
                         'product_uom': code,
                         }
-                # print(vals)
+
+                tax = ''
+                if line.get('TAX-CODE') == '1':
+                    tax = tax_ids.get(float(tax_code_ids.get(order_tax_code_ids.get(line.get('ORDER-NO')))))
+                    vals['tax_id'] = [(6, 0, [tax])]
 
                 res = sock.execute(DB, UID, PSW, 'sale.order.line', 'create', vals)
                 print(pid, 'Create - SALE ORDER LINE', order_id , res)
 
-        # except:
-        #     break
+        except Exception as e:
+            print(e)
 
 
 def sync_sale_order_lines():
@@ -66,7 +68,6 @@ def sync_sale_order_lines():
 
     fp = open('files/omlordr2.csv', 'r')
     csv_reader = csv.DictReader(fp)
-    print('Opened File')
 
     order_lines = {}
     for vals in csv_reader:
@@ -77,7 +78,6 @@ def sync_sale_order_lines():
             lines.append(vals)
 
     fp.close()
-    print('Closed File')
 
     data_pool = manager.list([{'order_id': order, 'lines': order_lines[order]} for order in order_lines])
 
@@ -89,14 +89,34 @@ def sync_sale_order_lines():
     uoms = sock.execute(DB, UID, PSW, 'uom.uom', 'search_read', [], ['id','name'])
     uom_ids = {uom['name']:uom['id'] for uom in uoms}
 
+    taxes = sock.execute(DB, UID, PSW, 'account.tax', 'search_read', [('name', '!=', 'Sale Tax' )], ['id','amount'])
+    tax1 = {float(tax['amount']): tax['id'] for tax in taxes}
+    tax_ids = manager.dict(tax1)
+
+    fp1 = open('files/fiscal.csv', 'r')
+    csv_reader1 = csv.DictReader(fp1)
+    tax_codes={}
+    for line in csv_reader1:
+        tax_codes[line.get('TAX-AUTH-CODE').strip()] = line.get('TAX-AUTH-PCT')
+    tax_code_ids = manager.dict(tax_codes)
+
+    fp2 = open('files/omlordr1.csv', 'r')
+    csv_reader2 = csv.DictReader(fp2)
+    oder_tax_codes={}
+    for line in csv_reader2:
+        oder_tax_codes[line.get('ORDER-NO').strip()] = line.get('TAX-AUTH-CODE')
+    order_tax_code_ids = manager.dict(oder_tax_codes)
+
     res = None
     order_ids = None
+    taxes = None
+    tax1 = None
     order_lines = None
     products = None
 
     for i in range(WORKERS):
         pid = "Worker-%d" % (i + 1)
-        worker = mp.Process(name=pid, target=update_sale_order_line, args=(pid, data_pool, error_ids, product_ids, uom_ids))
+        worker = mp.Process(name=pid, target=update_sale_order_line, args=(pid, data_pool, error_ids, product_ids, uom_ids, tax_code_ids, tax_ids, order_tax_code_ids))
         process_Q.append(worker)
         worker.start()
 
