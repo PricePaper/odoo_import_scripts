fe6327a122ae5cc7cfa4ab1fd811406112a76ac3
paul <paul.jose@confianzit.biz>
2020-10-06 16:30:36 +0530

    open invoice changes

--- 85_invoice.py
+++ 85_invoice.py
@@ -15,36 +15,33 @@ def update_invoice(pid, orders):
     while True:
         try:
             sock = xmlrpclib.ServerProxy(URL, allow_none=True)
-            data = orders.pop()
+            data = orders.get_nowait()
         except:
             break
         try:
-            res = sock.execute(DB, UID, PSW, 'sale.order', 'action_invoice_create', data, True)
-            print(pid, 'create - INVOICE', data, res)
+            inv = sock.execute(DB, UID, PSW, 'sale.order', 'action_create_open_invoice_xmlrpc', data)
+            print('Invoice created', pid, inv)
         except Exception as e:
-
-            print (e)
-            # orders.append(data)
-            continue
+            print (data, e)
 
 
 def sync_invoices():
     manager = mp.Manager()
-    orders = manager.list()
+    # orders = manager.list()
+    orders = manager.JoinableQueue()
     process_Q = []
 
     sock = xmlrpclib.ServerProxy(URL, allow_none=True)
     res = sock.execute(DB, UID, PSW, 'sale.order', 'search_read', [], ['note'])
     order_ids = {inv_no: rec['id'] for rec in res for inv_no in (rec['note'] or '').split(',')}
 
-    orders = []
     with open('files/rclopen1.csv', newline='') as f:
         csv_reader = csv.DictReader(f)
         for vals in csv_reader:
             inv_no = vals.get('INVOICE-NO', '').strip()
             order_id = order_ids.get(inv_no)
             if order_id:
-                orders.append(order_id)
+                orders.put(order_id)
 
 
     for i in range(WORKERS):
