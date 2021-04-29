8e81e2420a4ee61a8fbe06d798a462f31a2595be
Ean J. Price <ean@pricepaper.com>
2021-04-29 06:20:56 -0400

    Minox SQL fixes and restore 76_historical_so_done.sql from prev commit.

---
+++ 76_historical_so_done.sql
@@ -0,0 +1,2 @@
+UPDATE sale_order SET state='done',confirmation_date=q.date_order,release_date=q.date_order,deliver_by=q.date_order,effective_date=q.date_order,validity_date=q.date_order,commitment_date=q.date_order FROM (SELECT id,date_order FROM sale_order) AS q WHERE sale_order.id = q.id;
+UPDATE sale_order_line SET state = 'done';
--- 79_so_state_sale.sql
+++ 79_so_state_sale.sql
@@ -1,2 +1,2 @@
 UPDATE sale_order SET release_date=q.date_order,deliver_by=q.date_order,effective_date=q.date_order,validity_date=q.date_order,commitment_date=q.date_order FROM (SELECT id,date_order FROM sale_order) AS q WHERE sale_order.id = q.id AND state = 'draft';
-UPDATE sale_order_line l SET create_date = so.date_order FROM sale_order so WHERE l.order_id = so.id;
+UPDATE sale_order_line l SET create_date = so.date_order FROM sale_order so WHERE l.order_id = so.id AND l.state = 'draft';
