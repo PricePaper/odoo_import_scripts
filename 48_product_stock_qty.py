b67bf9842c1d7295e4e592d48eb378cfd3a82633
Ean J. Price <ean@pricepaper.com>
2021-04-29 22:01:10 -0400

    Improvements to #48 stocking inventory

--- 48_product_stock_qty.py
+++ 48_product_stock_qty.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import csv
 import multiprocessing as mp
-from xmlrpc import client as xmlrpclib
+import xmlrpc.client
+import logging.handlers
+
+# Get this using pip
+import multiprocessing_logging
 
 from scriptconfig import URL, DB, UID, PSW, WORKERS
+# Too many workers makes Odoo angry
+WORKERS = 4
 
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+filename = os.path.basename(__file__)
+logfile = os.path.splitext(filename)[0] + '.log'
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# create file handler which logs warn/errors to file
+errorlogfile = os.path.splitext(filename)[0] + '-error.log'
+eh = logging.FileHandler(errorlogfile, mode='w')
+eh.setLevel(logging.WARNING)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+eh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+logger.addHandler(eh)
+multiprocessing_logging.install_mp_handler(logger=logger)
 
 # ==================================== P R O D U C T S ====================================
 
 def update_product(pid, data_pool, product_ids, location_ids):
-    sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+    sock = xmlrpc.client.ServerProxy(URL, allow_none=True)
     while data_pool:
         try:
             data = data_pool.pop()
@@ -27,13 +59,22 @@ def update_product(pid, data_pool, product_ids, location_ids):
                         }
 
                 id = sock.execute(DB, UID, PSW, 'stock.change.product.qty', 'create', vals)
-                print(pid, 'create - line', id)
+                logger.debug(f'{pid}: Create stock move {default_code}')
                 res = sock.execute(DB, UID, PSW, 'stock.change.product.qty', 'change_product_qty', id, vals)
-                print(pid, 'Upadted - QTY', res)
+                logger.debug(f'{pid}: Update {default_code} qty {new_quantity}')
 
+        except xmlrpc.client.Fault as fault:
+            if fault.faultString.count('serialize'):
+                logger.warning('TransactionRollbackError - adding back to queue: ' + str(fault))
+                data_pool.append(data)
+            elif fault.faultString.count('Missing required fields'):
+                logger.error('Validation Error: {0}\n{1}'.format(fault, data))
+            else:
+                logger.error('Unknown XMLRPC Fault: {0}\nOffending line: {1}\n'.format(fault, data))
+                continue
         except Exception as e:
-            print(e)
-            #break
+            logger.critical('Unexpected exception: {0}\nOffending line:{1}\n'.format(e, data))
+            continue
 
 
 def sync_products():
@@ -47,7 +88,7 @@ def sync_products():
     fp1 = open('files/ivlioh.csv', 'r')
     # fp1 = open('ivlioh.csv', 'r')
     csv_reader1 = csv.DictReader(fp1)
-    sock = xmlrpclib.ServerProxy(URL, allow_none=True)
+    sock = xmlrpc.client.ServerProxy(URL, allow_none=True)
 
     all_locations = sock.execute(DB, UID, PSW, 'stock.location', 'search_read', [('usage', '=', 'internal')], ['id','name'])
     location_ids = {ele['name']:ele['id'] for ele in all_locations}
